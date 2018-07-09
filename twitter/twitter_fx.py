c6efcc2d1265ae0926e5bbb6fcadd77199107fff
Ogihara <pochi.0226.ogi@gmail.com>
2018-07-09 22:12:35 +0900

    Add files via upload

--- twitter/twitter_fx.py
+++ twitter/twitter_fx.py
@@ -3,6 +3,7 @@ from twitter_scraper import TwitterScraper
 import time
 import os
 import sys
+import logging
 
 """
 第一引数にスクロール数を受け取る
@@ -11,7 +12,7 @@ import sys
 search_tweet = ['為替','exchange','money order','dollar','rate','yen','euro','economics','経済','ドル']
 
 args = sys.argv
-
+logger = logging.getLogger()
 twitter_scraper = TwitterScraper()
 twitter_scraper.open_browser()
 twitter_scraper.logout()
@@ -23,6 +24,7 @@ while True:
         search_results = twitter_scraper.search_tweet(q,args[1])
         for search_result in search_results:
             twitter_db.add_record(search_result,'twitter',q)
+        logger.info(q,'is finised...exceed...')
         time.sleep(10)
     
 
